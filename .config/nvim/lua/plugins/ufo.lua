29b3911aaa8f6fde65adc487b7ac102346ee1d2c
Antonio Cicero Azevedo <azevedoantoniocicero@gmail.com>
2024-10-04 23:12:18 -0300

    add ufo plugin

---
+++ .config/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,45 @@
+local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, {suffix, 'MoreMsg'})
+    return newVirtText
+end
+
+return {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {'kevinhwang91/promise-async'},
+    config = function ()
+        local map = require('helpers.keys').map
+        local ufo = require('ufo')
+        map('n', 'zR', ufo.openAllFolds, 'Open all folds')
+        map('n', 'zM', ufo.closeAllFolds, 'Close all folds')
+        map('n', '<leader>zz', 'zA', 'Toggle fold')
+
+        ufo.setup {
+            provider_selector = function()
+                return {'treesitter', 'indent'}
+            end,
+            fold_virt_text_handler = handler
+        }
+    end
+}
